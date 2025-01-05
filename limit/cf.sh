c688966785a75ff29ffeda756d55559a08557721
Namikazeudin12 <158808428+Namikazeudin12@users.noreply.github.com>
2025-01-05 12:42:11 +0700

    Update cf.sh

--- limit/cf.sh
+++ limit/cf.sh
@@ -2,11 +2,11 @@
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 #read -p "Masukan Domain (contoh : Dragon)" domen
-DOMAIN=klmpk-tunneling.my.id
+DOMAIN=zntun.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.klmpk-tunneling.my.id
-CF_ID=andyyuda41@gmail.com
-CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+dns=${sub}.zntun.my.id
+CF_ID=fa4573620@gmail.com
+CF_KEY=8fe143822aaee5b7c477fa33a222f215b69ac
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
